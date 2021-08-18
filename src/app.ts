a671a4a47cdc1a013bd69bc67f6a450c12fa28b5
Alberto Marfoglia <alberto.marfoglia@studio.unibo.it>
2021-08-18 23:21:26 +0200

    add security middleware in app.js

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,18 @@ import express, {
 
 import morgan from 'morgan';
 import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
+import mongoSanitize from 'express-mongo-sanitize';
+import xss from 'xss-clean';
+import hpp from 'hpp';
 import AppError from './utils/appError';
 import userRouter from './routes/userRoutes';
 import globalErrorHandler from './controllers/errorController';
 
 const app: Application = express();
+
+// Set security HTTP headers
+app.use(helmet());
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -24,7 +31,18 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS (html code injection)
+app.use(xss());
+
+// Prevent parameter pollution (e.g double sort param)
+app.use(hpp({ whitelist: [] }));
+
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req: Request, res: Response, next: NextFunction) => {
