7694d2f644c9134ace197c2a515800a57479568e
Galde <federico.galdenzi@studio.unibo.it>
2021-09-09 12:56:58 +0200

    improved performance and fixed bug in articleController and articleDetailsController

--- src/controllers/articleController.ts
+++ src/controllers/articleController.ts
@@ -1,9 +1,10 @@
-import { NextFunction, Request, Response } from 'express';
+ import { NextFunction, Request, Response } from 'express';
 import catchAsync from '../utils/catchAsync';
 import HandlerFactory from './helpers/handlerFactory';
 import ArticleModel, { ArticleDoc } from '../models/articles/articleModel';
 import AppError from '../utils/appError';
 import Article from '../models/articles/article';
+import { isCategoryArticle, isCategoryType } from '../models/category';
 
 const factory = new HandlerFactory<ArticleDoc>();
 
@@ -11,9 +12,16 @@ class ArticleController {
   addArticle = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const article = req.body as Article;
     article.storeId = req.params.id;
+    // check if categoryArticle is a type of ours
+    if (!isCategoryArticle(article.category.categoryArticle)
+      || !isCategoryType(article.category.categoryType)) {
+      next(new AppError('Category or CategoryType is not valid', 400));
+      return;
+    }
     const newArticle = await ArticleModel.create(article);
     if (!newArticle) {
       next(new AppError('Cannot create article', 500));
+      return;
     }
     res.status(201).json({
       status: 'success',
--- src/controllers/articleDetailsController.ts
+++ src/controllers/articleDetailsController.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import catchAsync from '../utils/catchAsync';
 import HandlerFactory from './helpers/handlerFactory';
 import ArticleDetailsModel, { ArticleDetailsDoc } from '../models/articles/articleDetailsModel';
-import { ArticleDetails } from '../models/articles/article';
+import { ArticleDetails, ArticleStock } from '../models/articles/article';
 import AppError from '../utils/appError';
 
 const factory = new HandlerFactory<ArticleDetailsDoc>();
@@ -11,18 +11,37 @@ class ArticleDetailsController {
   addRetailArticle = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const articleDetails : ArticleDetails = req.body;
     articleDetails.articleId = req.params.id;
+
     if (!articleDetails.articleId) {
       next(new AppError('the id of article is not defined', 400));
+      return;
     }
+
+    if (!this.checkArticleSize(articleDetails.stockArticles)) {
+      next(new AppError('Size must be unique, found duplicates in stockArticles', 400));
+      return;
+    }
+
     const newArticleDetails = await ArticleDetailsModel.create(articleDetails);
     if (!newArticleDetails) {
       next(new AppError('Cannot create article', 500));
+      return;
     }
     res.status(201).json({
       status: 'success',
       data: { newArticleDetails }
     });
   });
+
+  checkArticleSize = function (stockArticles: ArticleStock[]): boolean {
+    /* inserisce in un set tutti i size degli articleStock, se la lunghezza dell'array unique
+       è diversa dalla lunghezza dello stock article allora
+       c'è una ripetizione di size es: [{size: M, qnt: 10}, {size: M, qnt: 2}, {size: S, qnt:3}]
+       unique.lenght = 2 (cioè size M e S).
+    */
+    const unique = [...new Set(stockArticles.map((elem) => elem.size))];
+    return unique.length === stockArticles.length;
+  }
 
   getRetailArticle = factory.getOne(ArticleDetailsModel);
 
